ab479154c0d0e2b3d650280697f30512abc1bc01
subo <subo@kean.edu>
2018-06-30 10:39:23 +0800

    First Commit

---
+++ Assets/story_btn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class story_btn : MonoBehaviour,IVirtualButtonEventHandler
+{
+
+    public GameObject vbBtnObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("Videobtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("BIN Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("BIN Realesed");
+    }
+}
---
+++ Assets/vb_audioplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_audioplay : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject vbBtnObj;
+    public AudioSource StoryAudio;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("AudioPlaybtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        StoryAudio.Stop();
+        Debug.Log("BIN Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        StoryAudio.Play();
+        Debug.Log("BIN Realesed");
+    }
+}
